3334ff9d41934bcfb9c1f6b38f3d781ffaea9fee
GuillermoTeran <teranguillermo59@gmail.com>
2024-10-30 16:09:41 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('C:\\Users\\USER\\Desktop\\TRIPLETEN\\SPRINT 6\\PROYECTO\\vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir dispersion') # crear un botón
+        
+if scatter_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un grafico de dispersion
+    fig = px.scatter(car_data, x="odometer", y="price")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+import streamlit as st
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
+# crear una casilla de verificación
+build_scatter = st.checkbox('Construir un grafico de dispersion')
+
+if build_scatter: # si la casilla de verificación está seleccionada
+    st.write('Construir un grafico de dispersion para la columna odómetro')
